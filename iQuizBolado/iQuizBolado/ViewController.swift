b9fdbb13967ff3d8327f079d751c44abf5326977
Vinicius Pinheiro de Oliveira <viniciusp.oliveira@icloud.com>
2023-12-07 08:17:31 -0300

    first commit

---
+++ iQuizBolado/iQuizBolado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuizBolado
+//
+//  Created by Vinicius Pinheiro de Oliveira on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
