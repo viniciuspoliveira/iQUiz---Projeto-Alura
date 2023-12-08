2b99df62cd43495e9ba8e7ce7aca14428f06aaf1
Vinicius Pinheiro de Oliveira <viniciusp.oliveira@icloud.com>
2023-12-08 07:38:53 -0300

    Criacao segunda tela

---
+++ iQuizBolado/iQuizBolado/QuestaoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestaoViewController.swift
+//  iQuizBolado
+//
+//  Created by Vinicius Pinheiro de Oliveira on 08/12/23.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iQuizBolado/iQuizBolado/ViewController.swift
+++ iQuizBolado/iQuizBolado/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var botaoIniciarQuiz: UIButton!
+    
+    @IBOutlet var backgroundTela1: UIView!
+    @IBAction func botaoPressionado(_ sender: Any) {
+        
+        print("O botao foi pressionado")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configuraLayout()
+      
     }
 
-
+    func configuraLayout(){
+        botaoIniciarQuiz.layer.cornerRadius = 12.0
+        
+    }
+    
+  
 }
 
